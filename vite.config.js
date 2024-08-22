d160dbbff3f4ef459dc0f0f1f92ec645f6689dfc
Baraiboapex <31634799+Baraiboapex@users.noreply.github.com>
2024-08-22 18:06:29 -0400

    config fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/mobile-phone-email-sender",
+  base:"mobile-phone-email-sender",
   plugins: [vue()],
 })
