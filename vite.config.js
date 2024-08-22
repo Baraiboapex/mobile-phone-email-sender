53c06fe730c1566247ff22245915b9a4fd8dc222
Baraiboapex <31634799+Baraiboapex@users.noreply.github.com>
2024-08-22 17:07:13 -0400

    project clean-up

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/mobile-phone-email-sender",
   plugins: [vue()],
-  server: {
-    port: 3002,
-  },
 })
