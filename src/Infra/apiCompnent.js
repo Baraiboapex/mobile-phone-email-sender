710d17f58bfa22a22f112e9e9cfe5ebba3af367e
matth <matth@BARAIBOBOX>
2024-12-09 19:14:49 -0500

    WIP

--- src/Infra/apiCompnent.js
+++ src/Infra/apiCompnent.js
@@ -21,8 +21,8 @@ async function setupFetch({
                 console.log(body);
             }
             
-            console.log(`fetch(${url}, ${configToSend.toString()})`, JSON.stringify(configToSend));
-
+            console.log(`fetch(${url}, ${configToSend.toString()})`, configToSend.toString());
+            
             fetch(url, configToSend)
             .then((res)=>parseResponseData({resp:res, method}))
             .then(async (data)=>{
